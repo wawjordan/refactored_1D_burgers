71f4644cc813c89ed828cccf9328cef336b2cd52
wawjordan <wajordan@vt.edu>
2021-11-23 10:32:46 -0500

    Changed input parameters Added:  'xloc_out'     - x location for output of time stencil  'dirname_out'  - dirname for time stencil output  'filename_out' - filename for time stencil output  'relax'        - under-relaxation parameter for iterative correction step Added call to ete_solver_w_IC_v3 (under-relaxation experiments)

--- test/solver_testing/ETE_IC_solver_testing.m
+++ test/solver_testing/ETE_IC_solver_testing.m
@@ -3,17 +3,17 @@ clc; clear; close all;
 
 order = 4;
 
-N = 257;
-t0 = -2;
-tf = 2;
-dt = 0.4/8;
-ex_soln = burgers_exact_soln('unsteady_shock',64,[-4,4]);
+% N = 257;%8193;
+% t0 = -2;
+% tf = 2;
+% dt = 0.4/8;%0.4/256;
+% ex_soln = burgers_exact_soln('unsteady_shock',64,[-4,4]);
 
-% N = 257;
-% t0 = 0.1;
-% tf = 0.6;
-% dt = 0.025/2;
-% ex_soln = burgers_exact_soln('pulse_plus',64,[-2,2]);
+N = 1025;%257;
+t0 = 0.1;
+tf = 0.6;
+dt = 0.025/8;%2;
+ex_soln = burgers_exact_soln('pulse_plus',64,[-2,2]);
 
 x = linspace(ex_soln.xmin,ex_soln.xmax,N);
 grid = grid1D(x);
@@ -30,17 +30,19 @@ S.tf = tf;
 S.dt = dt;
 S.dx = mean(grid.dx);
 S.U_out_interval = 1;
-S.Uex_out_interval = 1;
-S.R_out_interval = 1;
+S.Uex_out_interval = 0;
+S.R_out_interval = 0;
 S.E_out_interval = 1;
 S.out_iters = [];
 S.Niters = 10;
 S.out_iters = 1:S.Niters;
-% S.stencil_size = 7;
+% S.stencil_size = 11;
 S.stencil_size = order+1+mod(order,2);
 S.integrator    = BDF2_type(grid,soln,S);
 S.ETEintegrator = BDF2_type(grid,Esoln,S);
+% S.ETEintegrator.max_newton_iter = 1;
 S.ETEintegratorIC = BDF2_type(grid,EsolnIC,S);
+% S.ETEintegratorIC.max_newton_iter = 1;
 S.LS_S = spatial_reconstruction(grid,S,order);
 S.LS_T = temporal_reconstruction(grid,S,order,'method','default');
 
@@ -55,7 +57,16 @@ S.LHS = @(u) jacobian(u,grid.dx,S.nu,grid.N);
 S.ETE_RHS = @(u,e,Ru,TE) ETE_residual(u,e,Ru,TE,grid.dx,S.nu,grid.N);
 S.ETE_LHS = @(u,e) ETE_jacobian(u,e,grid.dx,S.nu,grid.N);
 
-[Esoln,EsolnIC,soln,OUT,S,stencil] = ete_solver_w_IC(grid,Esoln,EsolnIC,soln,S);
+%%%
+S.xloc_out     = 257;% 3585;%897;%113; %(x=-0.5)
+S.dirname_out  = 'C:\Users\Will\Documents\MATLAB\VT_Research\new\post_processing\';
+S.filename_out = 'pulse_iteration_error_alg1_1025_relax10.dat';
+S.relax = 1;
+%%%
+
+[Esoln,EsolnIC,soln,OUT,S,stencil] = ete_solver_w_IC_v3(grid,Esoln,EsolnIC,soln,S);
+% [Esoln,EsolnIC,soln,OUT,S,stencil] = ete_solver_w_IC_v2(grid,Esoln,EsolnIC,soln,S);
+% [Esoln,EsolnIC,soln,OUT,S,stencil] = ete_solver_w_IC(grid,Esoln,EsolnIC,soln,S);
 
 function val = jacobian(u,dx,nu,N)
 val = zeros(N,3);
