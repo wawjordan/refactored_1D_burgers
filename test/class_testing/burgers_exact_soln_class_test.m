e610bcb12a68b390fe368a253ca2ae5f012a3c6b
wawjordan <wajordan@vt.edu>
2021-12-01 13:19:35 -0500

    Verifying initial conditions for exact solutions (for table in SciTech paper)

--- test/class_testing/burgers_exact_soln_class_test.m
+++ test/class_testing/burgers_exact_soln_class_test.m
@@ -8,9 +8,19 @@ clc; clear; close all;
 % plot(x,Uex)
 % end
 ex_soln = burgers_exact_soln('pulse_plus',64,[-2,2]);
-x = linspace(-2,2,129)';
+f = @(x) 10*x./(1+(2/sqrt(5))*exp(20*x.^2));
+% ex_soln1 = burgers_exact_soln('#1',64,[-4,4]);
+% ex_soln2 = burgers_exact_soln('#1mod',64,[-4,4],'Uref',1);
+% x = linspace(-4,4,1025)';
+x = linspace(-2,2,1025)';
+hold on
+% plot(x,ex_soln.eval(x,0.1),'k')
+plot(x,f(x)-ex_soln.eval(x,0.1),'r')
+hold off;
 hold on;
-for t = 0.1:0.1:0.6
-Uex = ex_soln.eval(x,t);
-plot(x,Uex)
+for t = 0:0.1:1
+Uex1 = ex_soln1.eval(x,t);
+Uex2 = ex_soln2.eval(x,t);
+plot(x,Uex1,'k')
+plot(x,Uex2)
 end
