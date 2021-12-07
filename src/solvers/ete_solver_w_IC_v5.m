1ae2fde6ad6b19e49f5adbbbeda75e993be8d5d7
wawjordan <wajordan@vt.edu>
2021-12-07 12:10:50 -0500

    Added alternate startup procedure using SDIRK2 (Not valid for ETE right now)

--- src/solvers/ete_solver_w_IC_v5.m
+++ src/solvers/ete_solver_w_IC_v5.m
@@ -95,27 +95,45 @@ OUT.ERR.EE     = cell(S.max_steps,S.Niters+1);
 S = dt_options(S);
 stencil = soln_stencil( S.stencil_size, S.N, S.Niters+1 );
 int_test = (isa(S.integrator,'BDF2_type'));
-if (int_test)
-    time = OUT.t(1)-OUT.dt;
-    Uex = S.ex_soln.eval(grid.x,time);
-    stencil = stencil.push(Uex,time);
-    S.integrator.um2 = Uex;
-end
-
+% if (int_test)
+%     time = OUT.t(1)-OUT.dt;
+%     Uex = S.ex_soln.eval(grid.x,time);
+%     stencil = stencil.push(Uex,time);
+%     S.integrator.um2 = Uex;
+% end
+% S.count = 2;
+% time = OUT.t(1);
+% Uex = S.ex_soln.eval(grid.x,time);
+% stencil = stencil.push(Uex,time);
+% if (int_test)
+%     S.integrator.um1 = Uex;
+% end
+% soln.U = Uex;
+% OUT = output_primal_stuff(OUT,S,Uex,Uex,soln.E,0,1);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 time = OUT.t(1);
 Uex = S.ex_soln.eval(grid.x,time);
 stencil = stencil.push(Uex,time);
-if (int_test)
-    S.integrator.um1 = Uex;
-end
 soln.U = Uex;
 OUT = output_primal_stuff(OUT,S,Uex,Uex,soln.E,0,1);
+if (int_test)
+    S.integrator.um2 = Uex;
+    [soln,OUT,S] = startup_primal(grid,soln,OUT,S);
+      stencil = stencil.push(soln.U,OUT.t(2));
+    S.count = 3;
+else
+    OUT = output_primal_stuff(OUT,S,Uex,Uex,soln.E,0,1);
+    S.count = 2;
+end
+
+
 OUT = output_ete_stuff(OUT,S,Esoln.U,0,1);
 for i = 1:S.Niters
-    OUT = output_ete_iter_stuff(OUT,S,0,Uex,1,i);
+    OUT = output_ete_iter_stuff(OUT,S,0,Uex,1,i+1);
 end
 %% Solve Primal to fill stencil
 [soln,stencil,OUT,S]  = populate_stencil(grid,soln,stencil,OUT,S);
+% [Esoln,stencil,OUT,S] = startup_ete(grid,Esoln,stencil,OUT,S);
 [Esoln,stencil,OUT,S] = populate_ete(grid,Esoln,stencil,OUT,S);
 [EsolnIC,stencil,OUT,S] = populate_iterates(grid,EsolnIC,stencil,OUT,S);
 
@@ -141,9 +159,84 @@ S.string_fmt = ...
     sprintf('t = %%#%d.%df (timestep: %%%dd/%%d)\\n',12,L1,L2);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [soln,OUT,S] = startup_primal(grid,soln,OUT,S)
+time = OUT.t(2);
+Uex = S.ex_soln.eval(grid.x,time);
+L_BC1 = S.L_BC1;                         % LHS BC, left boundary
+L_BC2 = S.L_BC2;                         % LHS BC, right boundary
+R_BC1 = @(u,i) S.R_BC1(u,i,Uex(1));      % RHS BC, left boundary
+R_BC2 = @(u,i) S.R_BC2(u,i,Uex(grid.N)); % RHS BC, right boundary
+u_old = soln.U;
+tmp_integrator = SDIRK2_type(grid,soln,S);
+[u_new,resnorm,S,~] = tmp_integrator.step(...
+    u_old, S, S.RHS, S.LHS, L_BC1, L_BC2, R_BC1, R_BC2 );
+
+soln.U = u_new;
+soln.E = soln.U - Uex;
+OUT = output_primal_stuff(OUT,S,soln.U,Uex,soln.E,resnorm,2);
+S.integrator.um1 = u_new;
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% [Esoln,stencil,OUT,S] = populate_ete(grid,Esoln,stencil,OUT,S)
+function [Esoln,stencil,OUT,S] = startup_ete(grid,Esoln,stencil,OUT,S)
+i = 2;
+time = stencil.t(i);
+U = stencil.U(:,i,1);
+L_BC1 = S.L_BC1;
+L_BC2 = S.L_BC2;
+R_BC1 = @(u,i) S.R_BC1(u,i,0);
+R_BC2 = @(u,i) S.R_BC2(u,i,0);
+RU = ss_residual(U,grid.dx,S.nu,grid.N);
+% Reconstruction from primal
+[u,dudt] = S.LS_T.eval(stencil,time,1);
+TE = ss_residual_cont(S,S.LS_S,u);
+TE = TE + dudt;
+RHS = @(e) S.ETE_RHS(U,e,RU,TE);
+LHS = @(e) S.ETE_LHS(U,e);
+
+e_old = Esoln.U;
+tmp_integrator = SDIRK2_type(grid,Esoln,S);
+[e_new,resnorm,S,~] = tmp_integrator.step(...
+    e_old, S, RHS, LHS, L_BC1, L_BC2, R_BC1, R_BC2 );
+S.ETEintegrator.um1 = e_new;
+
+% e_old = Esoln.U;
+% [e_new,resnorm,S,S.ETEintegrator] = S.ETEintegrator.step(...
+%     e_old, S, RHS, LHS, L_BC1, L_BC2, R_BC1, R_BC2 );
+Esoln.U = e_new;
+stencil.U(:,i,2) = U - e_new; % update next stencil with corrected solution
+OUT = output_ete_stuff(OUT,S,Esoln.U,resnorm,i);
+end
+% [Esoln,stencil,OUT,S] = populate_iterates(grid,Esoln,stencil,OUT,S)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [Esoln,stencil,OUT,S] = startup_iterate(grid,Esoln,stencil,OUT,S,j)
+i = 2;
+time = stencil.t(i);
+U = stencil.U(:,i,j+1);
+L_BC1 = S.L_BC1;
+L_BC2 = S.L_BC2;
+R_BC1 = @(u,i) S.R_BC1(u,i,0);
+R_BC2 = @(u,i) S.R_BC2(u,i,0);
+RU = ss_residual(U,grid.dx,S.nu,grid.N);
+% Reconstruction from primal
+[u,dudt] = S.LS_T.eval(stencil,time,j+1);
+TE = ss_residual_cont(S,S.LS_S,u);
+TE = TE + dudt;
+RHS = @(e) S.ETE_RHS(U,e,RU,TE);
+LHS = @(e) S.ETE_LHS(U,e);
+
+e_old = Esoln.U;
+tmp_integrator = SDIRK2_type(grid,Esoln,S);
+[e_new,~,S,~] = tmp_integrator.step(...
+    e_old, S, RHS, LHS, L_BC1, L_BC2, R_BC1, R_BC2 );
+Esoln.U = e_new;
+S.ETEintegratorIC.um1 = e_new;
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [soln,stencil,OUT,S] = populate_stencil(grid,soln,stencil,OUT,S)
 % 1st step is initial condition
-for i = 2:S.stencil_size % fill stencil with primal solution
+% for i = 2:S.stencil_size % fill stencil with primal solution
+for i = S.count:S.stencil_size % fill stencil with primal solution
     count = i;
     time = OUT.t(count);
     fprintf(S.string_fmt,time,count-1,S.max_steps-1);
@@ -166,6 +259,7 @@ end
 function [Esoln,stencil,OUT,S] = populate_ete(grid,Esoln,stencil,OUT,S)
 % 1st step is initial condition (error is 0)
 for i = 2:S.stencil_size % march forward with ETE solution
+% for i = S.count:S.stencil_size % march forward with ETE solution
     time = stencil.t(i);
     U = stencil.U(:,i,1);
     L_BC1 = S.L_BC1;
@@ -187,8 +281,6 @@ for i = 2:S.stencil_size % march forward with ETE solution
     stencil.U(:,i,2) = U - e_new; % update next stencil with corrected solution
     OUT = output_ete_stuff(OUT,S,Esoln.U,resnorm,i);
 end
-%     stencil = stencil.push(Esoln.U,time); % update stencil
-% %     OUT = output_ete_stuff(OUT,S,Esoln.U,Uex,soln.E,resnorm,count);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [Esoln,stencil,OUT,S] = populate_iterates(grid,Esoln,stencil,OUT,S)
@@ -198,8 +290,11 @@ for j = 1:S.Niters   % iterative correction steps
     %% zero out error estimates
     S.ETEintegratorIC.um2 = zeros(grid.N,1);
     S.ETEintegratorIC.um1 = zeros(grid.N,1);
-    Esoln.U = zeros(grid.N,1);
+%     [Esoln,stencil,OUT,S] = startup_iterate(grid,Esoln,stencil,OUT,S,j);
+%     estError(:,2) = Esoln.U;
+%     Esoln.U = zeros(grid.N,1);
     for i = 2:S.stencil_size % march forward in time with ETE solution
+%     for i = S.count:S.stencil_size % march forward in time with ETE solution
         U = stencil.U(:,i,j+1); % corrected solution stencil
         %% set BCS
         L_BC1 = S.L_BC1;
@@ -226,14 +321,17 @@ for j = 1:S.Niters   % iterative correction steps
         Esoln.U = e_new;
     end
     %% apply correction to stencil
-    stencil.U(:,:,j+1) = stencil.U(:,:,j+1) - estError;
+    UE1 = stencil.U(:,:,j+1) - estError;
+%     stencil.U(:,:,j+1) = stencil.U(:,:,j+1) - estError;
     %% output info
     for i = 2:S.stencil_size
-        UE = stencil.U(:,i,j+1); % corrected solution stencil
+        UE = UE1(:,i); % corrected solution stencil
+%         UE = stencil.U(:,i,j+1); % corrected solution stencil
         if (j<S.Niters) % propagate correction to next iteration(?)
-            stencil.U(:,:,j+2) = stencil.U(:,:,j+1);
+            stencil.U(:,:,j+2) = UE1;
+%             stencil.U(:,:,j+2) = stencil.U(:,:,j+1);
         end
-        OUT = output_ete_iter_stuff(OUT,S,resnorm,UE,i,j);
+        OUT = output_ete_iter_stuff(OUT,S,resnorm,UE,i,j+1);
     end
 end
 % for j = 1:S.Niters
@@ -244,41 +342,42 @@ end
 function [Esoln,stencil,OUT,S] = step_iterates(grid,Esoln,stencil,OUT,S,count)
 % 1st step is initial condition (error is 0)
 i = stencil.queue_length;
-for j = 1:S.Niters   % iterative correction steps
+for j = 1:S.Niters-1   % iterative correction steps
     S.ETEintegratorIC.um2 = (stencil.U(:,i-2,j)-stencil.U(:,i-2,j+1));
     S.ETEintegratorIC.um1 = (stencil.U(:,i-1,j)-stencil.U(:,i-1,j+1));
+    e_old = (stencil.U(:,i,j)-stencil.U(:,i,j+1));
 %     S.ETEintegratorIC.um2 = (stencil.U(:,i-2,j)-stencil.U(:,i-2,j+1));
 %     S.ETEintegratorIC.um1 = (stencil.U(:,i-1,j)-stencil.U(:,i-1,j+1));
-    e_old = (stencil.U(:,i,j)-stencil.U(:,i,j+1));
+%     e_old = (stencil.U(:,i,j)-stencil.U(:,i,j+1));
     time = stencil.t(i);
-    U1 = stencil.U(:,i,1); % uncorrected solution stencil
-    U = stencil.U(:,i,j); % corrected solution stencil
+    U = stencil.U(:,i,j); % corrected solution stencil at previous iterate(?)
     L_BC1 = S.L_BC1;
     L_BC2 = S.L_BC2;
     R_BC1 = @(u,n) S.R_BC1(u,n,0); % dirichlet BC
     R_BC2 = @(u,n) S.R_BC2(u,n,0); % dirichlet BC
     RU = ss_residual(U,grid.dx,S.nu,grid.N);
-    [u,dudt] = S.LS_T.eval(stencil,time,j);
+%     stencil.U(:,i,j+1) = stencil.U(:,i,j); % use previous iteration for reconstruction
+    [u,dudt] = S.LS_T.eval(stencil,time,j); % reconstruction at current iteration level
     TE = ss_residual_cont(S,S.LS_S,u);
     TE = TE + dudt;
     RHS = @(e) S.ETE_RHS(U,e,RU,TE);
     LHS = @(e) S.ETE_LHS(U,e);
     [e_new,resnorm,S,S.ETEintegratorIC] = S.ETEintegratorIC.step(...
         e_old, S, RHS, LHS, L_BC1, L_BC2, R_BC1, R_BC2 );
-    ee = e_new-e_old;
+%     ee = e_new-e_old;
 %     if dot(ee,ee) > dot(e_old,e_old)
-%         e_new = e_old;
-%         rel = 0.1;%*dot(e_old,e_old)/dot(ee,ee);
+%         rel = 0.5;%*dot(e_old,e_old)/dot(ee,ee);
 %     else
 %         rel = 1;
 %     end
-    Esoln.U = e_new;
-    UE = U - e_new; % corrected solution
-    stencil.U(:,i,j+1) = UE; % update stencil with corrected solution
+    rel = 1;
+    Esoln.U = rel*e_new;
+    UE = U - rel*e_new; % corrected solution
+%     stencil.U(:,i,j+1) = UE; % update stencil with corrected solution
     if (j<S.Niters)
         stencil.U(:,i,j+2) = UE; % update stencil with corrected solution
     end
-    OUT = output_ete_iter_stuff(OUT,S,resnorm,UE,count,j);
+    OUT = output_ete_iter_stuff(OUT,S,resnorm,UE,count,j+1);
 end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -333,16 +432,16 @@ function OUT = output_ete_stuff(OUT,S,E,resnorm,count)
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function OUT = output_ete_iter_stuff(OUT,S,resnorm,UE,count,iter)
-    OUT.ERR.Rnorm{count,iter+1}  = resnorm;
+    OUT.ERR.Rnorm{count,iter}  = resnorm;
     E2 = OUT.PRI.U{count}- UE;
     tmp = abs(E2-OUT.PRI.E{count});
-    OUT.ERR.EnormX(count,iter+1,1) = sum(tmp)/S.N;          % L1
-    OUT.ERR.EnormX(count,iter+1,2) = sqrt(sum(tmp.^2)/S.N); % L2
-    OUT.ERR.EnormX(count,iter+1,3) = max(tmp);              % L3
-    if any(OUT.iters==iter)
+    OUT.ERR.EnormX(count,iter,1) = sum(tmp)/S.N;          % L1
+    OUT.ERR.EnormX(count,iter,2) = sqrt(sum(tmp.^2)/S.N); % L2
+    OUT.ERR.EnormX(count,iter,3) = max(tmp);              % L3
+    if any(OUT.iters==iter-1)
         if (S.E_out_interval~=0)&&(mod(count-1,S.E_out_interval) == 0)
-            OUT.ERR.E{count,iter+1}  = E2;
-            OUT.ERR.EE{count,iter+1} = E2-OUT.PRI.E{count};
+            OUT.ERR.E{count,iter}  = E2;
+            OUT.ERR.EE{count,iter} = E2-OUT.PRI.E{count};
         end
     end
 end
