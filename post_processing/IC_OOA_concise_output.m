08444083125056468bc921cd183b7b673f33a65c
wawjordan <wajordan@vt.edu>
2021-12-07 12:05:08 -0500

    Changed src_fname Added call to parse_IC_OOA_local_error and parse_IC_OOA_error_in_error

--- post_processing/IC_OOA_concise_output.m
+++ post_processing/IC_OOA_concise_output.m
@@ -4,7 +4,7 @@ src_dirname = [...
     'C:\Users\Will\Documents\MATLAB\VT_Research',...
     '\new\',...
     '\post_processing\'];
-src_fname = 'move_shock_ETEIC_newalg_mod';
+src_fname = 'shock_exact_start_alg1_allsave';
 % src_fname = 'ETE-IC-BD-4_pulseplus_test';
 % src_dirname = [...
 %     'C:\Users\Will\Documents\MATLAB\VT_Research',...
@@ -21,12 +21,19 @@ target_dirname = 'C:\Users\Will\Documents\MATLAB\VT_Research\new\post_processing
 %     '\new',...
 %     '\p',...
 %     '\tecplot_output\'];
-[SE1,SE2,PE1,PE2,DEBUG] = parse_IC_OOA_concise([src_dirname,src_fname]);
+
+
+% [SE] = parse_IC_OOA_local_error(src_fname);
+[SE] = parse_IC_OOA_local_error_in_error(src_fname);
+format_for_tecplot(target_dirname,'Local_Error_in_Error_Shock_4097.dat',SE);
+
+% [SE1,SE2,PE1,PE2,DEBUG] = parse_IC_OOA_concise([src_dirname,src_fname]);
 % [SE,PE,DEBUG] = parse_IC_OOA_concise_time([src_dirname,src_fname]);
 % [SE,PE,DEBUG] = parse_IC_OOA_concise_time_fine([src_dirname,src_fname]);
+% src_fname = 'pulse_ETEIC_newalg';
 % format_for_tecplot(target_dirname,[src_fname,'L1_Error_fine.dat'],SE.L(1));
-src_fname = 'move_shock_ETEIC_newalg';
-format_for_tecplot(target_dirname,[src_fname,'L1_Error.dat'],SE1.L(1));
-format_for_tecplot(target_dirname,[src_fname,'L1_OOA.dat'],PE1.L(1));
+
+% format_for_tecplot(target_dirname,[src_fname,'L1_Error.dat'],SE1.L(1));
+% format_for_tecplot(target_dirname,[src_fname,'L1_OOA.dat'],PE1.L(1));
 
 % format_for_tecplot(target_dirname,['time_',src_fname,'L1_OOA.dat'],PE2.L(1));
