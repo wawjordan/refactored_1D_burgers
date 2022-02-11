a05fe2925b0edd7a9412f8575ad9227f6d37a4dc
wawjordan <wajordan@vt.edu>
2022-02-11 12:24:02 -0500

    Removed repeated data structures for x and t Commented out file save options for debugging Changed some input parameters

--- test/grid_refinement/grid_refinement_IC_v1.m
+++ test/grid_refinement/grid_refinement_IC_v1.m
@@ -11,7 +11,7 @@ IN.N_IC    = 10;
 
 IN.t0      = 0.1;
 IN.tf      = 1.1;
-Ns  = 2.^(7:12)+1;
+Ns  = 2.^(7:8)+1;
 dts = 0.025./(2.^(0:length(Ns)-1));
 IN.ex_soln = burgers_exact_soln('#1',64,[-4,4]);
 % IN.t0      = 0.1;
@@ -26,7 +26,7 @@ IN.ex_soln = burgers_exact_soln('#1',64,[-4,4]);
 % IN.ex_soln = burgers_exact_soln('unsteady_shock',64,[-4,4]);
 
 
-IN.Tmethod = 'sgolay';
+IN.Tmethod = 'svd';
 % IN.U_out  = 1; IN.UE_out = 1; IN.R_out = 1; IN.E_out = 1;
 M = length(Ns);
 OUT = struct();
@@ -34,58 +34,47 @@ OUT.tstart = IN.t0;
 OUT.tstop  = IN.tf;
 OUT.Nx = Ns;
 OUT.dx = zeros(M,1);
+OUT.x  =  cell(M,1);
+OUT.t  =  cell(M,1);
 OUT.dt = dts;
 OUT.Local_Error_P = struct();
 OUT.Error_Norms_P = struct();
 OUT.Final_Enorm_P = zeros(M,3);
-OUT.Final_Enorm2_P = zeros(M,3);
 OUT.Local_Error_E = struct();
 OUT.Error_Norms_E = struct();
-OUT.Final_Enorm_E = cell(M,1);
+OUT.Final_Enorm_E = zeros(M,IN.N_IC+1,3);
 intervals = uint16((OUT.tstop-OUT.tstart)./OUT.dt);
 out_interval = 1;
 for i = 1:40
     out_interval = max(out_interval,gcd(i,intervals(1)));
 end
 intervals = intervals/out_interval;
-IN.U_out = 1;
-IN.UE_out = 1;
+IN.U_out = 0;
+IN.UE_out = 0;
 IN.R_out = 0;
-IN.E_out = 1;
+IN.E_out = 0;
 for i = 1:M
-    
     IN.N = Ns(i);
     IN.dt = dts(i);
     [grid,soln,Esoln,EsolnIC,S] = setup_problem_v1(IN);
-    [Esoln,EsolnIC,soln,OUTPUT,S,stencil] = ETEIC_solver_alg1_exact_startup(grid,Esoln,EsolnIC,soln,S);
+    [Esoln,EsolnIC,soln,OUTPUT,S,stencil] = ETEIC_solver_alg1(grid,Esoln,EsolnIC,soln,S);
 %     [Esoln,EsolnIC,soln,OUTPUT,S,stencil] = ete_solver_w_IC_v4(grid,Esoln,EsolnIC,soln,S);
-    L = length(OUTPUT.t)-1;
-    for j = 1:IN.N_IC+1
-        Ef(j,1) = norm(OUTPUT.ERR.EnormX(2:L+1,j,1),1)/L;
-        Ef(j,2) = norm(OUTPUT.ERR.EnormX(2:L+1,j,2),2)/sqrt(L);
-        Ef(j,3) = norm(OUTPUT.ERR.EnormX(2:L+1,j,3),Inf);
-    end
     OUT.dx(i) = S.dx;
+    OUT.x{i}  = grid.x;
+    OUT.t{i}  = OUTPUT.t;
     OUT.Local_Error_P(i).E = OUTPUT.PRI.E;
     OUT.Local_Error_P(i).u = OUTPUT.PRI.U;
-    OUT.Local_Error_P(i).x = grid.x;
-    OUT.Local_Error_P(i).t = OUTPUT.t;
     OUT.Error_Norms_P(i).E = OUTPUT.PRI.EnormX;
-    OUT.Error_Norms_P(i).t = OUTPUT.t;
-    OUT.Final_Enorm_P(i,:) = OUTPUT.PRI.EnormX(end,:);
+    OUT.Final_Enorm_P(i,:) = OUTPUT.PRI.Enorm;
     
     OUT.Local_Error_E(i).Ee = OUTPUT.ERR.EE;
     OUT.Local_Error_E(i).e = OUTPUT.ERR.E;
-    OUT.Local_Error_E(i).x = grid.x;
-    OUT.Local_Error_E(i).t = OUTPUT.t;
     OUT.Error_Norms_E(i).E = OUTPUT.ERR.EnormX;
-    OUT.Error_Norms_E(i).t = OUTPUT.t;
-    OUT.Final_Enorm_E{i} = Ef;
-%     OUT.Final_Enorm_E{i} = OUTPUT.ERR.EnormX(end,:,:);
+    OUT.Final_Enorm_E(i,:,:) = OUTPUT.ERR.Enorm;
 end
-fname = [...
-    'C:\Users\Will\Documents\MATLAB\VT_Research',...
-    '\new\',...
-    '\post_processing\expand_exact_start_alg1_allsave'];
+% fname = [...
+%     'C:\Users\Will\Documents\MATLAB\VT_Research',...
+%     '\new\',...
+%     '\post_processing\expand_exact_start_alg1_allsave'];
 % save(fname,'OUT');
-save(fname, 'OUT', '-v7.3')
+% save(fname, 'OUT', '-v7.3')
