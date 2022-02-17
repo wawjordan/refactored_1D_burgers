def3b7f914c22f248a27c55ed2ab394d08182c0e
wawjordan <wajordan@vt.edu>
2022-02-17 15:08:00 -0500

    Changed parameters and saved outputs

--- test/grid_refinement/grid_refinement_IC_v3.m
+++ test/grid_refinement/grid_refinement_IC_v3.m
@@ -2,18 +2,25 @@
 clc; clear; close all;
 IN = struct();
 IN.order   = 4;
-IN.N_IC    = 2;
+IN.N_IC    = 10;
 
 % IN.t0      = -2;
 % IN.tf      = 1;
-% Ns  = 2.^(7:14)+1;
+% Ns  = 2.^(7:13)+1;
 % dts = 0.025./(2.^(0:length(Ns)-1));
 % IN.ex_soln = burgers_exact_soln('move_shock',64,[-4,4]);
 
-IN.t0      = 0.1;
-IN.tf      = 1.1;
-Ns  = 2.^(7:10)+1;
-dts = 0.025./(2.^(0:length(Ns)-1));
+% IN.t0      = 0.1;
+% IN.tf      = 1.1;
+% Ns  = 2.^(7:14)+1;
+% dts = 0.025./(2.^(0:length(Ns)-1));
+% IN.ex_soln = burgers_exact_soln('#1',64,[-4,4]);
+
+IN.t0      = 0.2;
+IN.tf      = 1.2;
+Ns  = 2.^(5:12)+1;
+% dts = 0.1./(2.^(0:length(Ns)-1));
+dts = 0.05./(2.^(0:length(Ns)-1));
 IN.ex_soln = burgers_exact_soln('#1',64,[-4,4]);
 
 % IN.t0      = 0.1;
@@ -24,8 +31,8 @@ IN.ex_soln = burgers_exact_soln('#1',64,[-4,4]);
 
 % IN.t0      = -2;
 % IN.tf      = 1;
-% Ns  = 2.^(7:10)+1;
-% dts = 0.05./(2.^(0:length(Ns)-1));
+% Ns  = 2.^(5:12)+1;
+% dts = 0.2./(2.^(0:length(Ns)-1));
 % IN.ex_soln = burgers_exact_soln('unsteady_shock',64,[-4,4]);
 
 
@@ -54,8 +61,8 @@ for i = 1:M
     IN.dt = dts(i);
     [grid,soln,Esoln,EsolnIC,S] = setup_problem_v1(IN);
 %     S.integrator.max_newton_iter = 1;
-%     [Esoln,EsolnIC,soln,OUTPUT,S,stencil] = ETEIC_solver_alg1(grid,Esoln,EsolnIC,soln,S);
-    [Esoln,EsolnIC,soln,OUTPUT,S,stencil] = ETEIC_solver_alg1_exact_startup(grid,Esoln,EsolnIC,soln,S);
+    [Esoln,EsolnIC,soln,OUTPUT,S,stencil] = ETEIC_solver_alg1(grid,Esoln,EsolnIC,soln,S);
+%     [Esoln,EsolnIC,soln,OUTPUT,S,stencil] = ETEIC_solver_alg1_exact_startup(grid,Esoln,EsolnIC,soln,S);
     OUT.dx(i) = S.dx;
     OUT.x{i}  = grid.x;
     OUT.t{i}  = OUTPUT.t;
@@ -69,15 +76,16 @@ for i = 1:M
 end
 P1 = zeros(M-1,3);
 %%
-tmp1 = OUT.Primal_Norms(1:end-1,:)./OUT.Primal_Norms(2:end,:);
-tmp2 =    OUT.ETE_Norms(1:end-1,:)./   OUT.ETE_Norms(2:end,:);
-tmp3 = OUT.ETE_IC_Norms(1:end-1,:)./OUT.ETE_IC_Norms(2:end,:);
-PP = log(tmp1)/log(2);
-EP = log(tmp2)/log(2);
-IP = log(tmp3)/log(2);
-% fname = [...
-%     'C:\Users\Will\Documents\MATLAB\VT_Research',...
-%     '\new\',...
-%     '\post_processing\expand_exact_start_alg1_allsave'];
-% save(fname,'OUT');
+% tmp1 = OUT.Primal_Norms(1:end-1,:)./OUT.Primal_Norms(2:end,:);
+% tmp2 =    OUT.ETE_Norms(1:end-1,:)./   OUT.ETE_Norms(2:end,:);
+% tmp3 = OUT.ETE_IC_Norms(1:end-1,:)./OUT.ETE_IC_Norms(2:end,:);
+% PP = log(tmp1)/log(2);
+% EP = log(tmp2)/log(2);
+% IP = log(tmp3)/log(2);
+fname = [...
+    'C:\Users\Will\Documents\MATLAB\VT_Research',...
+    '\new\',...
+    '\results\02_11\',...
+    'expansion_fan_GS103'];
+save(fname,'OUT');
 % save(fname, 'OUT', '-v7.3')
